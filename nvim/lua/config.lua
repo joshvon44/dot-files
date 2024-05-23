8b7b69c601ac6a095a0d2f64ff33e5cbb63a85da
Joshua VonSeggern <joshvon44@gmail.com>
2024-05-22 22:44:32 -0400

    Update nvim config to use lazy.nvim

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -1,3 +1,52 @@
+-- Install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+    "rust-lang/rust.vim",
+    "tpope/vim-sensible",
+    "jeffkreeftmeijer/vim-numbertoggle",
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    "neovim/nvim-lspconfig",
+    "kylechui/nvim-surround",
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.7',
+          dependencies = {
+              'nvim-lua/plenary.nvim',
+              { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+          }
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+})
+
+require("catppuccin").setup({
+    transparent_background = true,
+})
+
+vim.cmd.colorscheme "catppuccin"
+
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
@@ -10,7 +59,7 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   -- List of parsers to ignore installing (or "all")
-  ignore_install = { "javascript" },
+  -- ignore_install = { "javascript" },
 
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
@@ -40,7 +89,63 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
+require("nvim-surround").setup()
+require('telescope').load_extension('fzf')
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<space>f', builtin.find_files, {})
+vim.keymap.set('n', '<space>/', builtin.live_grep, {})
+vim.keymap.set('n', '<space>b', builtin.buffers, {})
+vim.keymap.set('n', '<space>h', builtin.help_tags, {})
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+--    vim.keymap.set('n', '<space>f', function()
+--      vim.lsp.buf.format { async = true }
+--    end, opts)
+  end,
+})
+
 lspconfig = require("lspconfig")
+-- Rust LSP config
+lspconfig.rust_analyzer.setup {
+  -- Server-specific settings. See `:help lspconfig-setup`
+  settings = {
+    ['rust-analyzer'] = {},
+  },
+}
+
+-- Python LSP config
 lspconfig.pylsp.setup {
 on_attach = custom_attach,
 settings = {
@@ -68,4 +173,3 @@ flags = {
 },
 capabilities = capabilities,
 }
-
--- vimrc
+++ vimrc
@@ -1,24 +1,24 @@
 " Plugins will be downloaded under the specified directory.
-call plug#begin(has('nvim') ? stdpath('data') . '/plugged' : '~/.vim/plugged')
+"call plug#begin(has('nvim') ? stdpath('data') . '/plugged' : '~/.vim/plugged')
 
 " Declare the list of plugins.
 if has('nvim')
-	Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
-	Plug 'https://github.com/neovim/nvim-lspconfig.git'
+"    Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+"    Plug 'https://github.com/neovim/nvim-lspconfig.git'
 else
-	Plug 'https://github.com/preservim/nerdtree.git'
-	Plug 'https://github.com/tpope/vim-fugitive.git'
+"    Plug 'https://github.com/preservim/nerdtree.git'
+"    Plug 'https://github.com/tpope/vim-fugitive.git'
 endif
-Plug 'rust-lang/rust.vim'
-" Plug 'https://github.com/preservim/vim-markdown.git'
-Plug 'https://github.com/tpope/vim-sensible.git'
-" This plugin makes it so the hybrid number layout is used, I'm not really a
-" fan of it though
-Plug 'https://github.com/jeffkreeftmeijer/vim-numbertoggle.git'
-Plug 'catppuccin/nvim', { 'as': 'catppuccin' }
+"Plug 'rust-lang/rust.vim'
+"" Plug 'https://github.com/preservim/vim-markdown.git'
+"Plug 'https://github.com/tpope/vim-sensible.git'
+"" This plugin makes it so the hybrid number layout is used, I'm not really a
+"" fan of it though
+"Plug 'https://github.com/jeffkreeftmeijer/vim-numbertoggle.git'
+"Plug 'catppuccin/nvim', { 'as': 'catppuccin' }
 
 " List ends here. Plugins become visible to Vim after this call.
-call plug#end()
+"call plug#end()
 
 " Enable line number count
 set number
@@ -31,36 +31,35 @@ nnoremap <C-u> <C-u>zz
 "Remove all trailing whitespace by pressing F5
 nnoremap <F5> :let _s=@/<Bar>:%s/\s\+$//e<Bar>:let @/=_s<Bar><CR>
 
-" Make an actual tab width 4 spaces
-set tabstop=4
-" Indents have width of 4
+" Use spaces rather than tabs, default is 4 spaces per tab
+set expandtab
 set shiftwidth=4
-" Set number of columns for a tab
 set softtabstop=4
+set tabstop=4
 
-" Set colorscheme for text editor
-colorscheme catppuccin-mocha
+" Case insensitive search, unless there is one uppercase letter
+set ignorecase smartcase
 
 if has('nvim')
-	" Don't let neovim try to load powerline
-	" for why this works see /usr/share/vim/vimfiles/plugin/powerline.vim
-	" Basically by setting this value to 1, it tells pwoerline.vim that
-	" powerline is already loaded and we don't need to try to load it
-	let g:powerline_loaded = 1
+    " Don't let neovim try to load powerline
+    " for why this works see /usr/share/vim/vimfiles/plugin/powerline.vim
+    " Basically by setting this value to 1, it tells pwoerline.vim that
+    " powerline is already loaded and we don't need to try to load it
+    let g:powerline_loaded = 1
 else
-	" vim only features
-	" Enable the mouse to be used
-	set mouse=a
-	" Give the editor a consistent powerline look to the terminal
-	python3 from powerline.vim import setup as powerline_setup
-	python3 powerline_setup()
-	python3 del powerline_setup
-	set laststatus=2 " Always display the statusline in all windows
-	set showtabline=2 " Always display the tabline, even if there is only one tab
-	set noshowmode " Hide the default mode text (e.g. -- INSERT -- below the statusline)
-	set t_Co=256
-	" Start NERDTree and put the cursor back in the other window.
-	autocmd VimEnter * NERDTree | wincmd p
-	" Exit Vim if NERDTree is the only window remaining in the only tab.
-	autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
+    " vim only features
+    " Enable the mouse to be used
+    set mouse=a
+    " Give the editor a consistent powerline look to the terminal
+    python3 from powerline.vim import setup as powerline_setup
+    python3 powerline_setup()
+    python3 del powerline_setup
+    set laststatus=2 " Always display the statusline in all windows
+    set showtabline=2 " Always display the tabline, even if there is only one tab
+    set noshowmode " Hide the default mode text (e.g. -- INSERT -- below the statusline)
+    set t_Co=256
+    " Start NERDTree and put the cursor back in the other window.
+    autocmd VimEnter * NERDTree | wincmd p
+    " Exit Vim if NERDTree is the only window remaining in the only tab.
+    autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
 endif
