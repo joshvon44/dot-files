eff0fd5c42d89ef7ebebd34223e8161255f63261
Joshua VonSeggern <joshvon44@gmail.com>
2024-05-27 22:12:14 -0400

    Add LSP Zero pluging for neovim LSP support

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,4 +1,31 @@
 return {
-    -- TODO - Configure LSP using lazy nvim
-    "neovim/nvim-lspconfig",
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    -- {'williamboman/mason.nvim'},
+    -- {'williamboman/mason-lspconfig.nvim'},
+
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        config = function()
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
+
+            lsp_zero.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp_zero.default_keymaps({buffer = bufnr})
+            end)
+
+            -- Set up LSP language servers
+            -- Python LSP
+            require('lspconfig').pylsp.setup({})
+
+            -- Rust LSP
+            require('lspconfig').rust_analyzer.setup({})
+        end,
+    },
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
 }
