ad111868c7e66b2408845fca0f49de6bb07c070d
Joshua VonSeggern <joshvon44@gmail.com>
2024-06-04 21:30:59 -0400

    Fix config for treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,40 +2,42 @@ return {
     {
         "nvim-treesitter/nvim-treesitter", 
         build = ":TSUpdate",
-        opts = {
-            ensure_installed = { 
-                "bash",
-                "c",
-                "doxygen",
-                "lua",
-                "make",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "rust",
-                "vim",
-                "vimdoc",
-                "zig",
-            },
+        config = function() 
+            require('nvim-treesitter.configs').setup {
+                ensure_installed = { 
+                    "bash",
+                    "c",
+                    "doxygen",
+                    "lua",
+                    "make",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "rust",
+                    "vim",
+                    "vimdoc",
+                    "zig",
+                },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
 
-            -- Automatically install missing parsers when entering buffer
-            auto_install = true,
+                -- Automatically install missing parsers when entering buffer
+                auto_install = true,
 
-            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-            highlight = {
-                enable = true,
+                highlight = {
+                    enable = true,
 
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-        }
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+            }
+        end
     },
 }
