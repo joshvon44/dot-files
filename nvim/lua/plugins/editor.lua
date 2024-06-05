3bb6d623b8bbacc318f38987b353b8ccbb9a17e7
Joshua VonSeggern <joshvon44@gmail.com>
2024-06-04 21:53:35 -0400

    Add Neovim Tree (file navigation)

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,11 +1,14 @@
 return {
+    -- Auto adjusting indentation
     { "tpope/vim-sleuth" },
+    -- Surround highlighted text 
     {
         "kylechui/nvim-surround",
         init = function()
             require("nvim-surround").setup()
         end,
     },
+    -- Provides hints for what key mappings do
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
@@ -17,6 +20,25 @@ return {
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
+        },
+        {
+            "nvim-tree/nvim-tree.lua",
+            version = "*",
+            lazy = false,
+            dependencies = {
+                "nvim-tree/nvim-web-devicons",
+            },
+            config = function()
+                require("nvim-tree").setup {}
+                local api = require("nvim-tree.api")
+
+                local function opts(desc)
+                    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+                end
+
+                vim.keymap.set('n', 'tt', api.tree.toggle, opts('Toggle nvim tree'))
+
+            end,
         }
     },
 }
